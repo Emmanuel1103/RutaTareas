751419cde25b962e5388e17b9ca3aca7c5aa0e8c
Emmanuel Morales <aprendizprocesos@fundacionsantodomingo.org>
2025-11-07 15:27:12 -0500

    Version4.0

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "RutaTareas", // 
+  base: "/RutaTareas/", 
   server: {
     host: "::",
     port: 8080,
